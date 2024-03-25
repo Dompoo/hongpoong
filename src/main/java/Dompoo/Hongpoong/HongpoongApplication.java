c951d11a0ccc11cb40a0d49e4dcec791a3418afc
Dompoo <dlckdrms0517@gmail.com>
2024-03-25 15:44:17 +0900

    24/03/25 졸업 프로젝트 시작

---
+++ src/main/java/Dompoo/Hongpoong/HongpoongApplication.java
@@ -0,0 +1,13 @@
+package Dompoo.Hongpoong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongpoongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongpoongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Dompoo/Hongpoong/HongpoongApplicationTests.java
@@ -0,0 +1,13 @@
+package Dompoo.Hongpoong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongpoongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

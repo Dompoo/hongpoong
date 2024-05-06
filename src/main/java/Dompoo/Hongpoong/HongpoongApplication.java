95f877d5884daccd4106bc41f7c1b4ed823a8878
Dompoo <dlckdrms0517@gmail.com>
2024-05-06 23:42:24 +0900

    1차 개발 완료

--- src/main/java/Dompoo/Hongpoong/HongpoongApplication.java
+++ src/main/java/Dompoo/Hongpoong/HongpoongApplication.java
@@ -1,11 +1,9 @@
 package Dompoo.Hongpoong;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class HongpoongApplication {
 
 	public static void main(String[] args) {

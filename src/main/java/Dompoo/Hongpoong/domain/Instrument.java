3c456851154a0a481c2e93bbcb5bffacbbbd5033
Dompoo <dlckdrms0517@gmail.com>
2024-05-09 20:57:34 +0900

    fix : 악기 개수 삭제

--- src/main/java/Dompoo/Hongpoong/domain/Instrument.java
+++ src/main/java/Dompoo/Hongpoong/domain/Instrument.java
@@ -14,16 +14,14 @@ public class Instrument {
     private Long id;
 
     private String product;
-    private int count;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
     private Member member;
 
     @Builder
-    public Instrument(String product, int count, Member member) {
+    public Instrument(String product, Member member) {
         this.product = product;
-        this.count = count;
         setMember(member);
     }
 

872dd6110f87123f60489a86a8607dff753f9838
Dompoo <dlckdrms0517@gmail.com>
2024-05-15 00:49:02 +0900

    refactor : 더 이상 사용하지 않는 클래스 삭제

--- src/main/java/Dompoo/Hongpoong/repository/ChatMessageRepository.java
+++
@@ -1,7 +0,0 @@
-//package Dompoo.Hongpoong.repository;
-//
-//import Dompoo.Hongpoong.domain.ChatMessage;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-//}
--- src/main/java/Dompoo/Hongpoong/repository/RentalRepository.java
+++
@@ -1,7 +0,0 @@
-//package Dompoo.Hongpoong.repository;
-//
-//import Dompoo.Hongpoong.domain.Rental;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RentalRepository extends JpaRepository<Rental, Long> {
-//}

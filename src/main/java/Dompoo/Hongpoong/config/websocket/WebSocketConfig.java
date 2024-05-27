ebea2f70040e62a84e711eb3deb408a5bf2b8e47
Dompoo <dlckdrms0517@gmail.com>
2024-05-28 02:09:02 +0900

    feat : STOMP 엔드포인트 수정

--- src/main/java/Dompoo/Hongpoong/config/websocket/WebSocketConfig.java
+++ src/main/java/Dompoo/Hongpoong/config/websocket/WebSocketConfig.java
@@ -13,14 +13,14 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.configureMessageBroker(registry);
-        registry.setApplicationDestinationPrefixes("/app"); //클라이언트 -> 서버
-        registry.enableSimpleBroker("/room"); //서버 -> 클라이언트, /room/id 를 구독한 사람이 메세지를 받는다.
+        registry.setApplicationDestinationPrefixes("/chat"); //클라이언트 -> 서버
+        registry.enableSimpleBroker("/chat/room"); //서버 -> 클라이언트, /room/id 를 구독한 사람이 메세지를 받는다.
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         WebSocketMessageBrokerConfigurer.super.registerStompEndpoints(registry);
-        registry.addEndpoint("/chat") //websocket 연결 수립
+        registry.addEndpoint("/ws") //websocket 연결 수립
                 .withSockJS();
     }
 }

6696698fc4a3959765a63bf95cf439287795b5b9
Dompoo <dlckdrms0517@gmail.com>
2024-08-26 20:06:31 +0900

    fix : Enum 클래스 분리

---
+++ src/main/java/Dompoo/Hongpoong/domain/enums/Club.java
@@ -0,0 +1,5 @@
+package Dompoo.Hongpoong.domain.enums;
+
+public enum Club {
+	HWARANG, SANTLE, AKBAN, DEULNEOK, ETC;
+}
---
+++ src/main/java/Dompoo/Hongpoong/domain/enums/InstrumentType.java
@@ -0,0 +1,5 @@
+package Dompoo.Hongpoong.domain.enums;
+
+public enum InstrumentType {
+	KKWANGGWARI, JANGGU, BUK, SOGO, JING;
+}

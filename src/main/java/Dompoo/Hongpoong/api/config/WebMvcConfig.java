59866f60fdb594258c91692a5a39bec598899e60
Dompoo <dlckdrms0517@gmail.com>
2024-09-04 17:50:19 +0900

    feat: ArgumentHandler, Interceptor 등록

---
+++ src/main/java/Dompoo/Hongpoong/api/config/WebMvcConfig.java
@@ -0,0 +1,31 @@
+package Dompoo.Hongpoong.api.config;
+
+import Dompoo.Hongpoong.common.security.AuthInterceptor;
+import Dompoo.Hongpoong.common.security.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebMvcConfig implements WebMvcConfigurer {
+	
+	private final AuthInterceptor authInterceptor;
+	private final LoginUserArgumentResolver loginUserArgumentResolver;
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(authInterceptor);
+		WebMvcConfigurer.super.addInterceptors(registry);
+	}
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(loginUserArgumentResolver);
+		WebMvcConfigurer.super.addArgumentResolvers(resolvers);
+	}
+}

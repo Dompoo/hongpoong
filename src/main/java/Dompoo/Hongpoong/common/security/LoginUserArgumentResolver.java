56a2029c1d06caaee353817f273f3af52e896227
Dompoo <dlckdrms0517@gmail.com>
2024-09-04 12:20:14 +0900

    feat: LoginUser에 값을 채워줄 ArgumentResolver 작성

---
+++ src/main/java/Dompoo/Hongpoong/common/security/LoginUserArgumentResolver.java
@@ -0,0 +1,26 @@
+package Dompoo.Hongpoong.common.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+	
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		return parameter.hasParameterAnnotation(LoginUser.class)
+				&& parameter.getParameterType() == UserClaims.class;
+	}
+	
+	@Override
+	public UserClaims resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+		return (UserClaims) webRequest.getAttribute("claims", RequestAttributes.SCOPE_REQUEST);
+	}
+}

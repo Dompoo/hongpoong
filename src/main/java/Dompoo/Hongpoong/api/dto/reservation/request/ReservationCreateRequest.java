478df5f058adf70b3653837bf1892e6fe154f537
Dompoo <dlckdrms0517@gmail.com>
2024-09-09 02:05:14 +0900

    docs: reservation 관련 dto들에 swagger 적용

--- src/main/java/Dompoo/Hongpoong/api/dto/reservation/request/ReservationCreateRequest.java
+++ src/main/java/Dompoo/Hongpoong/api/dto/reservation/request/ReservationCreateRequest.java
@@ -3,12 +3,14 @@ package Dompoo.Hongpoong.api.dto.reservation.request;
 import Dompoo.Hongpoong.domain.entity.Member;
 import Dompoo.Hongpoong.domain.entity.reservation.Reservation;
 import Dompoo.Hongpoong.domain.entity.reservation.ReservationTime;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Getter
@@ -18,20 +20,27 @@ import java.util.List;
 public class ReservationCreateRequest {
 
     @NotNull(message = "인원수를 입력하세요.")
+    @Schema(example = "10")
     private final Integer number;
 
+    @NotNull(message = "예약 날짜를 입력하세요.")
     @FutureOrPresent(message = "과거 날짜일 수 없습니다.")
+    @Schema(example = "2024-04-17")
     private final LocalDate date;
 
     @NotBlank(message = "시작 시간을 입력하세요.")
-    private final String startTime;
+    @Schema(example = "10:00:00")
+    private final LocalTime startTime;
 
     @NotBlank(message = "종료 시간을 입력하세요.")
-    private final String endTime;
+    @Schema(example = "21:00:00")
+    private final LocalTime endTime;
     
     @NotNull(message = "참가자를 입력하세요.")
+    @Schema(example = "{1, 2, 3}")
     private final List<Long> participaterIds;
-
+    
+    @Schema(example = "산틀 정공 연습")
     private final String message = "";
     
     public Reservation toReservation(Member creator) {
--- src/main/java/Dompoo/Hongpoong/api/dto/reservation/response/ReservationResponse.java
+++ src/main/java/Dompoo/Hongpoong/api/dto/reservation/response/ReservationResponse.java
@@ -2,6 +2,7 @@ package Dompoo.Hongpoong.api.dto.reservation.response;
 
 import Dompoo.Hongpoong.domain.entity.ReservationParticipate;
 import Dompoo.Hongpoong.domain.entity.reservation.Reservation;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -9,21 +10,39 @@ import lombok.Getter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Getter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ReservationResponse {
-
+    
+    @Schema(example = "1")
     private final Long id;
+    
+    @Schema(example = "이창근")
     private final String username;
+    
+    @Schema(example = "email@gmail.com")
     private final String email;
+    
+    @Schema(example = "10")
     private final Integer number;
+    
+    @Schema(example = "2024-04-17")
     private final LocalDate date;
-    private final String startTime;
-    private final String endTime;
+    
+    @Schema(example = "10:00:00")
+    private final LocalTime startTime;
+    
+    @Schema(example = "21:00:00")
+    private final LocalTime endTime;
+    
+    @Schema(example = "산틀 정공 연습")
     private final String message;
+    
+    @Schema(example = "2024-04-17T20:00:00")
     private final LocalDateTime lastmodified;
     
     public static ReservationResponse from(Reservation reservation) {
@@ -33,8 +52,8 @@ public class ReservationResponse {
                 .email(reservation.getCreator().getEmail())
                 .number(reservation.getNumber())
                 .date(reservation.getDate())
-                .startTime(reservation.getStartTime().strValue)
-                .endTime(reservation.getEndTime().strValue)
+                .startTime(reservation.getStartTime().localTime)
+                .endTime(reservation.getEndTime().localTime)
                 .message(reservation.getMessage())
                 .lastmodified(reservation.getLastModified())
                 .build();

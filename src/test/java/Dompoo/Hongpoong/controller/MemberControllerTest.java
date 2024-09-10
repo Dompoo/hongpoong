fa6fb32b489102a0c4a9ae551524ffd40b9f498e
Dompoo <dlckdrms0517@gmail.com>
2024-09-10 11:18:28 +0900

    test: Member 컨트롤러/서비스 테스트 수정/보강

--- src/test/java/Dompoo/Hongpoong/controller/MemberControllerTest.java
+++ src/test/java/Dompoo/Hongpoong/controller/MemberControllerTest.java
@@ -5,6 +5,7 @@ import Dompoo.Hongpoong.api.dto.member.request.MemberRoleEditRequest;
 import Dompoo.Hongpoong.api.dto.member.response.MemberResponse;
 import Dompoo.Hongpoong.api.dto.member.response.MemberStatusResponse;
 import Dompoo.Hongpoong.config.MyWebMvcTest;
+import Dompoo.Hongpoong.domain.enums.Role;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -20,23 +21,75 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class MemberControllerTest extends MyWebMvcTest {
 
-    private final Long ID  = 1L;
-    private final Long ID2  = 2L;
-    private final String EMAIL = "dompoo@gmail.com";
-    private final String EMAIL2 = "yoonH@gmail.com";
-    private final String NAME = "이창근";
-    private final String NAME2 = "강윤호";
-    private final String NICKNAME = "불꽃남자";
-    private final String NICKNAME2 = "물남자";
-    private final String CLUB = "산틀";
-    private final String CLUB2 = "악반";
-    private final String NEW_USERNAME = "dompoo2";
-    private final String NEW_PASSWORD = "qwer";
-    private final Integer ENROLLMENT_NUMBER = 19;
-    private final Integer ENROLLMENT_NUMBER2 = 18;
-    private final String PROFILE_IMAGE_URL = "image.com/1";
-    private final String PROFILE_IMAGE_URL2 = "image.com/2";
-    private final Boolean PUSH_ALARM = true;
+    private static final Long ID  = 1L;
+    private static final Long ID2  = 2L;
+    private static final String EMAIL = "dompoo@gmail.com";
+    private static final String EMAIL2 = "yoonH@gmail.com";
+    private static final String NAME = "이창근";
+    private static final String NAME2 = "강윤호";
+    private static final String NICKNAME = "불꽃남자";
+    private static final String NICKNAME2 = "물남자";
+    private static final String CLUB = "산틀";
+    private static final String CLUB2 = "악반";
+    private static final String NEW_USERNAME = "dompoo2";
+    private static final String NEW_PASSWORD = "qwer";
+    private static final Integer ENROLLMENT_NUMBER = 19;
+    private static final Integer ENROLLMENT_NUMBER2 = 18;
+    private static final String PROFILE_IMAGE_URL = "image.com/1";
+    private static final String PROFILE_IMAGE_URL2 = "image.com/2";
+    private static final Boolean PUSH_ALARM = true;
+    private static final String ROLE = Role.MEMBER.korName;
+    private static final String ROLE2 = Role.LEADER.korName;
+    
+    //회원 전체 조회
+    @Test
+    @DisplayName("회원 전체 조회")
+    void test() throws Exception {
+        //given
+        when(memberService.findAllMember()).thenReturn(List.of(
+                MemberResponse.builder()
+                        .memberId(ID)
+                        .email(EMAIL)
+                        .name(NAME)
+                        .nickname(NICKNAME)
+                        .club(CLUB)
+                        .enrollmentNumber(ENROLLMENT_NUMBER)
+                        .profileImageUrl(PROFILE_IMAGE_URL)
+                        .role(ROLE)
+                        .build(),
+                MemberResponse.builder()
+                        .memberId(ID2)
+                        .email(EMAIL2)
+                        .name(NAME2)
+                        .nickname(NICKNAME2)
+                        .club(CLUB2)
+                        .enrollmentNumber(ENROLLMENT_NUMBER2)
+                        .profileImageUrl(PROFILE_IMAGE_URL2)
+                        .role(ROLE2)
+                        .build()
+        ));
+        
+        //expected
+        mockMvc.perform(get("/member"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.size()").value(2))
+                .andExpect(jsonPath("$[0].memberId").value(ID))
+                .andExpect(jsonPath("$[0].email").value(EMAIL))
+                .andExpect(jsonPath("$[0].name").value(NAME))
+                .andExpect(jsonPath("$[0].nickname").value(NICKNAME))
+                .andExpect(jsonPath("$[0].club").value(CLUB))
+                .andExpect(jsonPath("$[0].enrollmentNumber").value(ENROLLMENT_NUMBER))
+                .andExpect(jsonPath("$[0].profileImageUrl").value(PROFILE_IMAGE_URL))
+                .andExpect(jsonPath("$[0].role").value(ROLE))
+                .andExpect(jsonPath("$[1].memberId").value(ID2))
+                .andExpect(jsonPath("$[1].email").value(EMAIL2))
+                .andExpect(jsonPath("$[1].name").value(NAME2))
+                .andExpect(jsonPath("$[1].nickname").value(NICKNAME2))
+                .andExpect(jsonPath("$[1].club").value(CLUB2))
+                .andExpect(jsonPath("$[1].enrollmentNumber").value(ENROLLMENT_NUMBER2))
+                .andExpect(jsonPath("$[1].profileImageUrl").value(PROFILE_IMAGE_URL2))
+                .andExpect(jsonPath("$[1].role").value(ROLE2));
+    }
 
     //로그인 정보
     @Test
@@ -99,67 +152,8 @@ class MemberControllerTest extends MyWebMvcTest {
                 .andExpect(status().isOk())
                 .andDo(print());
     }
-
-    //회원 리스트 - 관리자
-    @Test
-    @DisplayName("회원 리스트 조회")
-    void getList() throws Exception {
-        //given
-        when(memberService.findAllMember()).thenReturn(List.of(
-                MemberResponse.builder()
-                        .memberId(ID)
-                        .email(EMAIL)
-                        .name(NAME)
-                        .nickname(NICKNAME)
-                        .club(CLUB)
-                        .enrollmentNumber(ENROLLMENT_NUMBER)
-                        .profileImageUrl(PROFILE_IMAGE_URL)
-                        .build(),
-                MemberResponse.builder()
-                        .memberId(ID2)
-                        .email(EMAIL2)
-                        .name(NAME2)
-                        .nickname(NICKNAME2)
-                        .club(CLUB2)
-                        .enrollmentNumber(ENROLLMENT_NUMBER2)
-                        .profileImageUrl(PROFILE_IMAGE_URL2)
-                        .build()
-        ));
-
-        //expected
-        mockMvc.perform(get("/member"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.size()").value(2))
-                .andExpect(jsonPath("$[0].memberId").value(ID))
-                .andExpect(jsonPath("$[0].email").value(EMAIL))
-                .andExpect(jsonPath("$[0].name").value(NAME))
-                .andExpect(jsonPath("$[0].nickname").value(NICKNAME))
-                .andExpect(jsonPath("$[0].club").value(CLUB))
-                .andExpect(jsonPath("$[0].enrollmentNumber").value(ENROLLMENT_NUMBER))
-                .andExpect(jsonPath("$[0].profileImageUrl").value(PROFILE_IMAGE_URL))
-                .andExpect(jsonPath("$[1].memberId").value(ID2))
-                .andExpect(jsonPath("$[1].email").value(EMAIL2))
-                .andExpect(jsonPath("$[1].name").value(NAME2))
-                .andExpect(jsonPath("$[1].nickname").value(NICKNAME2))
-                .andExpect(jsonPath("$[1].club").value(CLUB2))
-                .andExpect(jsonPath("$[1].enrollmentNumber").value(ENROLLMENT_NUMBER2))
-                .andExpect(jsonPath("$[1].profileImageUrl").value(PROFILE_IMAGE_URL2))
-                .andDo(print());
-    }
-
-    //회원삭제 - 관리자
-    @Test
-    @DisplayName("회원삭제")
-    void deleteAdmin() throws Exception {
-        //given
-
-        //expected
-        mockMvc.perform(delete("/member/manage/{id}", ID))
-                .andExpect(status().isOk())
-                .andDo(print());
-    }
-
-    //권한 변경 - 관리자
+    
+    //권한 변경
     @Test
     @DisplayName("권한 변경")
     void editMemberRole() throws Exception {
@@ -167,16 +161,59 @@ class MemberControllerTest extends MyWebMvcTest {
         MemberRoleEditRequest request = MemberRoleEditRequest.builder()
                 .role("패짱")
                 .build();
-
+        
         String json = objectMapper.writeValueAsString(request);
-
+        
+        //expected
+        mockMvc.perform(patch("/member/{id}", ID)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andDo(print());
+        
+    }
+    
+    //회원삭제
+    @Test
+    @DisplayName("회원삭제")
+    void deleteAdmin() throws Exception {
+        //given
+        
+        //expected
+        mockMvc.perform(delete("/member/{id}", ID))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+    
+    //권한 변경 - 관리자
+    @Test
+    @DisplayName("어드민 권한 변경")
+    void editMemberRoleByAdmin() throws Exception {
+        //given
+        MemberRoleEditRequest request = MemberRoleEditRequest.builder()
+                .role("패짱")
+                .build();
+        
+        String json = objectMapper.writeValueAsString(request);
+        
         //expected
         mockMvc.perform(patch("/member/manage/{id}", ID)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
                 .andExpect(status().isOk())
                 .andDo(print());
-
+        
     }
 
+    //회원삭제 - 관리자
+    @Test
+    @DisplayName("어드민 회원삭제")
+    void deleteAdminByAdmin() throws Exception {
+        //given
+
+        //expected
+        mockMvc.perform(delete("/member/manage/{id}", ID))
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
 }
--- src/test/java/Dompoo/Hongpoong/service/MemberServiceTest.java
+++ src/test/java/Dompoo/Hongpoong/service/MemberServiceTest.java
@@ -12,7 +12,6 @@ import Dompoo.Hongpoong.domain.enums.Club;
 import Dompoo.Hongpoong.domain.enums.Role;
 import Dompoo.Hongpoong.domain.repository.MemberRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +21,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
+import static Dompoo.Hongpoong.domain.enums.Club.AKBAN;
 import static Dompoo.Hongpoong.domain.enums.Club.SANTLE;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,57 +40,70 @@ class MemberServiceTest {
     private static final String NAME = "이창근";
     private static final String NICKNAME = "불꽃남자";
     private static final String PASSWORD = "1234";
-    private static final String NEW_USERNAME = "새로운이름";
-    private static final String NEW_PASSWORD = "asdf";
     private static final Club CLUB = SANTLE;
+    private static final Role ROLE = Role.MEMBER;
     private static final Integer ENROLLMENT_NUMBER = 19;
     private static final String PROFILE_IMAGE_URL = "image.com/1";
-
-    private Member member;
     
-    @BeforeEach
-    void setUp() {
-        member = memberRepository.save(Member.builder()
-                .email(EMAIL)
-                .name(NAME)
-                .password(encoder.encode(PASSWORD))
-                .club(SANTLE)
-                .build());
-    }
+    private static final String EMAIL2 = "yoonH@gmail.com";
+    private static final String NAME2 = "새로운이름";
+    private static final String PASSWORD2 = "asdf";
+    private static final String NICKNAME2 = "물남자";
+    private static final Club CLUB2 = AKBAN;
+    private static final Integer ENROLLMENT_NUMBER2 = 18;
+    private static final String PROFILE_IMAGE_URL2 = "image.com/2";
     
     @AfterEach
     void tearDown() {
         memberRepository.deleteAllInBatch();
+    }
+    
+    @Test
+    @DisplayName("회원 리스트 조회")
+    void findAllMember() {
+        //given
+        memberRepository.save(Member.builder()
+                .email(EMAIL)
+                .name(NAME)
+                .password(PASSWORD)
+                .club(SANTLE)
+                .build());
+        
+        memberRepository.save(Member.builder()
+                .email(EMAIL)
+                .name(NAME2)
+                .password(PASSWORD2)
+                .club(SANTLE)
+                .build());
+        
+        //when
+        List<MemberResponse> list = service.findAllMember();
+        
+        //then
+        assertEquals(2, list.size());
+        assertEquals(NAME, list.get(0).getName());
+        assertEquals(NAME2, list.get(1).getName());
     }
 
     @Test
     @DisplayName("로그인 정보")
     void findMyMemberDetail() {
         //given
-        Member member = memberRepository.save(Member.builder()
-                .email(EMAIL)
-                .name(NEW_USERNAME)
-                .password(NEW_PASSWORD)
-                .club(SANTLE)
-                .build());
+        Member member = memberRepository.save(buildMember());
 
         //when
         MemberStatusResponse response = service.findMyMemberDetail(member.getId());
 
         //then
         assertEquals(response.getEmail(), EMAIL);
-        assertEquals(response.getName(), NEW_USERNAME);
+        assertEquals(response.getName(), NAME);
     }
-
+    
     @Test
     @DisplayName("존재하지 않는 회원의 로그인 정보")
     void findMyMemberDetailFail() {
         //given
-        Member member = memberRepository.save(Member.builder()
-                .email(EMAIL)
-                .name(NEW_USERNAME)
-                .password(NEW_PASSWORD)
-                .build());
+        Member member = memberRepository.save(buildMember());
 
         //when
         MemberNotFound e = assertThrows(MemberNotFound.class, () ->
@@ -102,16 +115,18 @@ class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("멤버 정보 전체 수정")
-    void editMyMember1() {
+    @DisplayName("회원정보 수정")
+    void editMyMember() {
         //given
+        Member member = memberRepository.save(buildMember());
+        
         MemberEditDto dto = MemberEditDto.builder()
-                .name(NEW_USERNAME)
-                .newPassword(NEW_PASSWORD)
-                .profileImageUrl(PROFILE_IMAGE_URL)
-                .enrollmentNumber(ENROLLMENT_NUMBER)
-                .club(CLUB)
-                .nickname(NICKNAME)
+                .name(NAME2)
+                .nickname(NICKNAME2)
+                .newPassword(PASSWORD2)
+                .profileImageUrl(PROFILE_IMAGE_URL2)
+                .enrollmentNumber(ENROLLMENT_NUMBER2)
+                .club(CLUB2)
                 .build();
 
         //when
@@ -119,16 +134,18 @@ class MemberServiceTest {
 
         //then
         assertEquals(memberRepository.findAll().getFirst().getEmail(), "dompoo@gmail.com");
-        assertEquals(memberRepository.findAll().getFirst().getName(), NEW_USERNAME);
-        assertTrue(encoder.matches(NEW_PASSWORD, memberRepository.findAll().getFirst().getPassword()));
+        assertEquals(memberRepository.findAll().getFirst().getName(), NAME2);
+        assertTrue(encoder.matches(PASSWORD2, memberRepository.findAll().getFirst().getPassword()));
     }
 
     @Test
-    @DisplayName("존재하는 아이디로 멤버 수정 시도")
+    @DisplayName("존재하지 않는 회원정보 수정 시도")
     void editMyMemberFail2() {
+        Member member = memberRepository.save(buildMember());
+        
         MemberEditRequest request = MemberEditRequest.builder()
-                .name(NEW_USERNAME)
-                .password(NEW_PASSWORD)
+                .name(NAME2)
+                .password(PASSWORD2)
                 .build();
 
         //when
@@ -142,7 +159,10 @@ class MemberServiceTest {
 
     @Test
     @DisplayName("회원탈퇴")
-    void deleteMemberByAdmin() {
+    void deleteMyMember() {
+        //given
+        Member member = memberRepository.save(buildMember());
+        
         //when
         service.deleteMemberByAdmin(member.getId());
 
@@ -153,6 +173,9 @@ class MemberServiceTest {
     @Test
     @DisplayName("존재하지 않는 회원아이디로 회원탈퇴")
     void deleteMemberByAdminFail1() {
+        //given
+        Member member = memberRepository.save(buildMember());
+        
         //when
         MemberNotFound e = assertThrows(MemberNotFound.class,
                 () -> service.deleteMemberByAdmin(member.getId() + 1));
@@ -161,29 +184,77 @@ class MemberServiceTest {
         assertEquals(e.getMessage(), "존재하지 않는 유저입니다.");
         assertEquals(e.statusCode(), "404");
     }
-
+    
     @Test
-    @DisplayName("회원 리스트 조회")
-    void findAllMember() {
+    @DisplayName("내 패 회원 권한 변경")
+    void editMemberAuth() {
         //given
-        memberRepository.save(Member.builder()
-                .email(EMAIL)
-                .name(NEW_USERNAME)
-                .password(NEW_PASSWORD)
-                .club(SANTLE)
-                .build());
-
+        Member me = memberRepository.save(buildMember());
+        Member target = memberRepository.save(buildMember2());
+        
+        MemberRoleEditRequest request = MemberRoleEditRequest.builder()
+                .role(Role.LEADER.korName)
+                .build();
+        
         //when
-        List<MemberResponse> list = service.findAllMember();
-
+        service.editMemberAuth(me.getId(), target.getId(), request.toDto());
+        
         //then
-        assertEquals(2, list.size());
-        assertEquals(NAME, list.get(0).getName());
-        assertEquals(NEW_USERNAME, list.get(1).getName());
+        assertEquals(memberRepository.findAll().getFirst().getRole().name(), "LEADER");
+    }
+    
+    @Test
+    @DisplayName("존재하지 않는 회원 권한 변경")
+    void editMemberAuthFail() {
+        //given
+        Member me = memberRepository.save(buildMember());
+        Member target = memberRepository.save(buildMember2());
+        
+        MemberRoleEditRequest request = MemberRoleEditRequest.builder()
+                .role(Role.LEADER.korName)
+                .build();
+        
+        //when
+        MemberNotFound e = assertThrows(MemberNotFound.class,
+                () -> service.editMemberAuth(me.getId(), target.getId() + 1, request.toDto()));
+        
+        //then
+        assertEquals(e.getMessage(), "존재하지 않는 유저입니다.");
+        assertEquals(e.statusCode(), "404");
+    }
+    
+    @Test
+    @DisplayName("내 패 회원 삭제")
+    void deleteMember() {
+        //given
+        Member me = memberRepository.save(buildMember());
+        Member target = memberRepository.save(buildMember2());
+        
+        //when
+        service.deleteMember(me.getId(), target.getId());
+        
+        //then
+        assertEquals(memberRepository.findAll().size(), 1);
+    }
+    
+    @Test
+    @DisplayName("존재하지 않는 회원 권한 변경")
+    void deleteMemberFail() {
+        //given
+        Member me = memberRepository.save(buildMember());
+        Member target = memberRepository.save(buildMember2());
+        
+        //when
+        MemberNotFound e = assertThrows(MemberNotFound.class,
+                () -> service.deleteMember(me.getId(), target.getId());
+        
+        //then
+        assertEquals(e.getMessage(), "존재하지 않는 유저입니다.");
+        assertEquals(e.statusCode(), "404");
     }
 
     @Test
-    @DisplayName("회원 권한 변경")
+    @DisplayName("어드민 회원 권한 변경")
     void editMemberAuthByAdmin() {
         //given
         Member find = memberRepository.findAll().getFirst();
@@ -200,7 +271,7 @@ class MemberServiceTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 회원 권한 변경")
+    @DisplayName("어드민 존재하지 않는 회원 권한 변경")
     void editMemberAuthByAdminFail() {
         //given
         Member find = memberRepository.findAll().getFirst();
@@ -216,5 +287,61 @@ class MemberServiceTest {
         //then
         assertEquals(e.getMessage(), "존재하지 않는 유저입니다.");
         assertEquals(e.statusCode(), "404");
+    }
+    
+    @Test
+    @DisplayName("어드민 회원 삭제")
+    void deleteMemberByAdmin() {
+        //given
+        Member target = memberRepository.save(buildMember2());
+        
+        //when
+        service.deleteMemberByAdmin(target.getId());
+        
+        //then
+        assertEquals(memberRepository.findAll().size(), 1);
+    }
+    
+    @Test
+    @DisplayName("어드민 존재하지 않는 회원 권한 변경")
+    void deleteMemberByAdminFail() {
+        //given
+        Member target = memberRepository.save(buildMember2());
+        
+        //when
+        MemberNotFound e = assertThrows(MemberNotFound.class,
+                () -> service.deleteMemberByAdmin(target.getId());
+        
+        //then
+        assertEquals(e.getMessage(), "존재하지 않는 유저입니다.");
+        assertEquals(e.statusCode(), "404");
+    }
+    
+    private Member buildMember() {
+        return Member.builder()
+                .email(EMAIL)
+                .name(NAME)
+                .nickname(NICKNAME)
+                .password(encoder.encode(PASSWORD))
+                .club(CLUB)
+                .role(ROLE)
+                .enrollmentNumber(ENROLLMENT_NUMBER)
+                .profileImageUrl(PROFILE_IMAGE_URL)
+                .pushAlarm(true)
+                .build();
+    }
+    
+    private Member buildMember2() {
+        return Member.builder()
+                .email(EMAIL2)
+                .name(NAME2)
+                .nickname(NICKNAME2)
+                .password(encoder.encode(PASSWORD2))
+                .club(CLUB)
+                .role(ROLE)
+                .enrollmentNumber(ENROLLMENT_NUMBER2)
+                .profileImageUrl(PROFILE_IMAGE_URL2)
+                .pushAlarm(true)
+                .build();
     }
 }

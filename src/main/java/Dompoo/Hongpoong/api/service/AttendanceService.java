00d7d5128119979434f9f5a2db7bf36253055c9e
Dompoo <dlckdrms0517@gmail.com>
2024-09-11 20:40:17 +0900

    fix: 출석 닫을 시 해당 예약의 생성자가 아니면 닫을 수 없도록 수정

--- src/main/java/Dompoo/Hongpoong/api/service/AttendanceService.java
+++ src/main/java/Dompoo/Hongpoong/api/service/AttendanceService.java
@@ -2,9 +2,13 @@ package Dompoo.Hongpoong.api.service;
 
 import Dompoo.Hongpoong.api.dto.attendance.AttendanceResponse;
 import Dompoo.Hongpoong.common.exception.impl.AttendanceNotFound;
+import Dompoo.Hongpoong.common.exception.impl.EditFailException;
+import Dompoo.Hongpoong.common.exception.impl.ReservationNotFound;
+import Dompoo.Hongpoong.domain.entity.Reservation;
 import Dompoo.Hongpoong.domain.entity.ReservationParticipate;
 import Dompoo.Hongpoong.domain.enums.Attendance;
 import Dompoo.Hongpoong.domain.repository.ReservationParticipateRepository;
+import Dompoo.Hongpoong.domain.repository.ReservationRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,6 +23,7 @@ import static Dompoo.Hongpoong.domain.enums.Attendance.NO_SHOW;
 public class AttendanceService {
 
     private final ReservationParticipateRepository reservationParticipateRepository;
+    private final ReservationRepository reservationRepository;
     
     @Transactional(readOnly = true)
     public List<AttendanceResponse> findAttendance(Long reservationId) {
@@ -39,7 +44,13 @@ public class AttendanceService {
     }
     
     @Transactional
-    public List<AttendanceResponse> closeAttendance(Long reservationId) {
+    public List<AttendanceResponse> closeAttendance(Long memberId, Long reservationId) {
+        Reservation reservation = reservationRepository.findByIdJoinFetchCreator(reservationId).orElseThrow(ReservationNotFound::new);
+        
+        if (!reservation.getCreator().getId().equals(memberId)) {
+            throw new EditFailException();
+        }
+        
         List<ReservationParticipate> reservationParticipates = reservationParticipateRepository.findByReservationIdAndNotAttend(reservationId);
         
         reservationParticipates.forEach(rp -> rp.editAttendance(NO_SHOW));
--- src/main/java/Dompoo/Hongpoong/domain/repository/ReservationRepository.java
+++ src/main/java/Dompoo/Hongpoong/domain/repository/ReservationRepository.java
@@ -2,11 +2,18 @@ package Dompoo.Hongpoong.domain.repository;
 
 import Dompoo.Hongpoong.domain.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    
+    @Query("SELECT r FROM Reservation r JOIN FETCH r.creator WHERE r.id = :reservationId")
+    Optional<Reservation> findByIdJoinFetchCreator(@Param("reservationId") Long reservationId);
+    
     List<Reservation> findAllByDate(LocalDate date);
     List<Reservation> findAllByDateBetween(LocalDate startDate, LocalDate endDate);
 }

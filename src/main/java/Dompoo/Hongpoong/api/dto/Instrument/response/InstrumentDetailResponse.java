6150c30b530a961d01e0253aa1434eb83a1a150c
Dompoo <dlckdrms0517@gmail.com>
2024-09-11 19:52:49 +0900

    feat: InstrumentDetailResponse에 대여자 정보를 더 자세하게 표현

--- src/main/java/Dompoo/Hongpoong/api/dto/Instrument/response/InstrumentDetailResponse.java
+++ src/main/java/Dompoo/Hongpoong/api/dto/Instrument/response/InstrumentDetailResponse.java
@@ -1,5 +1,6 @@
 package Dompoo.Hongpoong.api.dto.Instrument.response;
 
+import Dompoo.Hongpoong.api.dto.member.response.MemberResponse;
 import Dompoo.Hongpoong.domain.entity.Instrument;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
@@ -24,9 +25,6 @@ public class InstrumentDetailResponse {
     @Schema(example = "산틀")
     private final String club;
     
-    @Schema(example = "1")
-    private final Long borrowerId;
-    
     @Schema(example = "false")
     private final Boolean available;
     
@@ -36,15 +34,17 @@ public class InstrumentDetailResponse {
     @Schema(example = "20:00:00")
     private final LocalTime returnTime;
     
+    private final MemberResponse borrower;
+    
     public static InstrumentDetailResponse from(Instrument instrument) {
         return InstrumentDetailResponse.builder()
                 .instrumentId(instrument.getId())
                 .type(instrument.getType().korName)
                 .club(instrument.getClub().korName)
-                .borrowerId(instrument.getBorrower().getId())
+                .borrower(instrument.getBorrower() == null ? null : MemberResponse.from(instrument.getBorrower()))
                 .available(instrument.isAvailable())
-                .returnDate(instrument.getReservation().getDate())
-                .returnTime(instrument.getReservation().getEndTime().localTime)
+                .returnDate(instrument.getReservation() == null ? null : instrument.getReservation().getDate())
+                .returnTime(instrument.getReservation() == null ? null : instrument.getReservation().getEndTime().localTime)
                 .build();
     }
 }
--- src/main/java/Dompoo/Hongpoong/domain/repository/InstrumentRepository.java
+++ src/main/java/Dompoo/Hongpoong/domain/repository/InstrumentRepository.java
@@ -17,6 +17,6 @@ public interface InstrumentRepository extends JpaRepository<Instrument, Long> {
 	@Query("select i from Instrument i where i.borrower.club = :club")
 	List<Instrument> findAllByClubEquals(@Param("club") Club club);
 	
-	@Query("SELECT i FROM Instrument i JOIN FETCH i.borrower WHERE i.id = :instrumentId")
-	Optional<Instrument> findByInstrumentIdFetchJoinBorrower(@Param("instrumentId") Long instrumentId);
+	@Query("SELECT i FROM Instrument i JOIN FETCH i.borrower JOIN FETCH i.reservation WHERE i.id = :instrumentId")
+	Optional<Instrument> findByInstrumentIdFetchJoinBorrowerAndReservation(@Param("instrumentId") Long instrumentId);
 }
